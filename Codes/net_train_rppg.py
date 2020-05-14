250ce6f07dc759d5c980d42fba647476039ef799
mxahan <smxahsan@gmail.com>
2020-05-14 00:59:53 -0400

    new

--- Codes/net_train_rppg.py
+++ Codes/net_train_rppg.py
@@ -33,10 +33,20 @@ from sklearn.model_selection import train_test_split
 #iD_ir = '../../../Dataset/Merl_Tim/Subject1_still/IR'
 
 #iD_ir = '../../../Dataset/Merl_Tim/Subject1_still/RGB_raw'
-iD_ir = '../../../Dataset/Merl_Tim/Subject1_still/RGB_demosaiced'
+#iD_ir = '../../../Dataset/Merl_Tim/Subject1_still/RGB_demosaiced'
 
+path_dir = '../../../Dataset/Merl_Tim'
+
+subjects = ['/Subject1_still', '/Subject2_still', '/Subject3_still']
+
+im_mode = ['/IR', '/RGB_raw', '/RGB_demosaiced']
+
+
+
+iD_ir = path_dir + subjects[1]+im_mode[2]
 
 dataPath = os.path.join(iD_ir, '*.pgm')
+
 files = glob.glob(dataPath)  # care about the serialization
 # end load pathdir
 list.sort(files) # serialing the data
@@ -59,7 +69,7 @@ for f1 in files:  # make sure to stack serially
 data =  np.array(data)
     
 #%% load Mat file
-x = loadmat('../../../Dataset/Merl_Tim/Subject1_still/PulseOX/pulseOx.mat')
+x = loadmat(path_dir + subjects[1]+'/PulseOX/pulseOx.mat')
 
 pulseoxR = np.squeeze(x['pulseOxRecord'])
 
@@ -70,12 +80,15 @@ for i in range(pulseoxR.shape[0]):
 pulR = np.array(pulR)
     
 #%% Prepare dataset for training
+# For subject one go till 5300
+# For suject 2 go till 6230
+
 random.seed(1)
 
-rv = [randint(0,5300) for _ in range(5000)]
+rv = [randint(0,6230) for _ in range(5000)]
 randint(0,5350)
 rv =  np.array(rv)
-pulR = np.reshape(pulR, [10703,1])
+pulR = np.reshape(pulR, [pulR.shape[0],1])
 #      #%%
 
 trainX = []
@@ -101,7 +114,7 @@ num_classes = 80 # total classes (0-9 digits).
 num_features = 100*100*40 # data features (img shape: 28*28).
 
 # Training parameters.
-learning_rate = 0.001 # start with 0.001
+learning_rate = 0.0001 # start with 0.001
 training_steps = 30000
 batch_size = 8
 display_step = 100
@@ -120,8 +133,7 @@ trX, teX, trY, teY = train_test_split(trainX , trainY, test_size = .1, random_st
 #%% tensorflow dataload
 
 train_data = tf.data.Dataset.from_tensor_slices((trX, trY))
-train_data = train_data.repeat().shuffle(buffer_size=5, seed= 1).batch(batch_size).prefetch(1)
-
+train_data = train_data.repeat().shuffle(buffer_size=500, seed= 8).batch(batch_size).prefetch(1)
 
 
 #%% Loss function  
@@ -157,24 +169,26 @@ def train_nn(neural_net, train_data):
       
 
 #%% Bringing Network
-from net_work_def import ConvNet
+from net_work_def import ConvNet, ConvNet1
+#%% load network
 neural_net = ConvNet(num_classes)
-
-
+Basenet = ConvNet1(num_classes)
 #%% Training the actual network
 inarg = (neural_net, train_data)
-train_nn(*inarg)
+with tf.device('gpu:0/'):
+    train_nn(*inarg)
 
 #%% Model weight  save
-neural_net.save_weights('../../../Dataset/Merl_Tim/NNsave/SavedWM/weights/my_checkpoint')
+#neural_net.save_weights('../../../Dataset/Merl_Tim/NNsave/SavedWM/weights/my_checkpoint')
 
 #%% Load weight load
-neural_net.load_weights(
+Basenet.load_weights(
     '../../../Dataset/Merl_Tim/NNsave/SavedWM/weights/my_checkpoint')
 
 #%% Random testing
 
-i = 94
+i = 17
+
 
 #trX1 = np.reshape(data[i:i+40,:,:,0], [40,100,100])
 #trX1 = np.moveaxis(trX1, 0,-1)
@@ -197,6 +211,13 @@ trX1 = (trX1 - trX1.min())/(trX1.max() - trX1.min())
 predd = neural_net(trX1) 
 plt.plot(predd[0])
 
+#%% Seeing inside the network
+in1 = neural_net.layers[0](trX1).numpy() # plt.plot(in1[0,:,:,1])
+in2 = neural_net.layers[1](in1).numpy() # plt.plot(in2[0,:,:,1])  
+in3 = neural_net.layers[9](in2).numpy()
+# well we can go on
+
+
 #%%  Extras
 
 for i in range(40):
@@ -205,3 +226,5 @@ for i in range(40):
     plt.show()
     
     
+#%% Get_weights and Set_weights
+neural_net.layers[5].set_weights(Basenet.layers[5].get_weights())
--- Codes/net_work_def.py
+++ Codes/net_work_def.py
@@ -30,6 +30,69 @@ class ConvBNRelu(keras.Model):
         
         return x 
     
+## Initial Network works fine but 41M  parameters the problem with dense layers
+class ConvNet1(Model): # Vitamon network except inception layer
+    # Set layers.
+    def __init__(self, num_classes):
+        super(ConvNet1, self).__init__()
+        # Convolution Layer with 32 filters and a kernel size of 5.
+        self.conv1 = ConvBNRelu(32)
+        # Convolution Layer with 64 filters and a kernel size of 3.
+        self.conv2 = ConvBNRelu(64)
+        # Max Pooling (down-sampling) with kernel size of 2 and strides of 2. 
+        self.maxpool1 = layers.MaxPool2D(2, strides=2)
+        
+        self.conv3 = ConvBNRelu(32, kernel_size=3)
+        # Max Pooling (down-sampling) with kernel size of 2 and strides of 2. 
+
+        # Convolution Layer with 64 filters and aBasenet kernel size of 3.
+        self.conv4 = ConvBNRelu(64, kernel_size=3)
+        # Max Pooling (down-sampling) with kernel size of 2 and strides of 2. 
+        self.maxpool2 = layers.MaxPool2D(2, strides=2)
+
+        # Flatten the data to a 1-D vector for the fully connected layer.
+        self.flatten = layers.Flatten()
+
+        self.fc1 = layers.Dense(1024, activation=tf.nn.relu)
+        
+        self.fc2 = layers.Dense(512, activation=tf.nn.relu)
+        # Apply Dropout (if is_training is False, dropout is not applied).
+        # self.concat1 = layers.Concatenate()
+
+        # Output layer, class prediction.
+        self.out = layers.Dense(num_classes, tf.nn.relu)
+
+    # Set forward pass.
+    def call(self, x, training=False):
+  #       x = tf.reshape(x, [-1, 100, 100, 40])
+        # xl = []
+        # for i in range(4):
+        #     x1 = x[:,:,:,i*10:(i+1)*10]
+        #     x2 = self.conv1(x1, training=training)
+        #     xl.append(x2)
+        
+
+        # # print(x1.shape)
+        
+        # x =self.concat1(xl)
+        
+        x = self.conv1(x, training=training)
+        # print(x.shape)
+        
+        x = self.conv2(x, training=training)
+        x = self.maxpool1(x)
+        x = self.conv3(x, training=training)
+        x = self.conv4(x, training=training)
+        
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.out(x)
+
+        return x
+
+# Network Option  2 
 
 class ConvNet(Model): # Vitamon network except inception layer
     # Set layers.
@@ -49,13 +112,12 @@ class ConvNet(Model): # Vitamon network except inception layer
         self.conv4 = ConvBNRelu(64, kernel_size=3)
         # Max Pooling (down-sampling) with kernel size of 2 and strides of 2. 
         self.maxpool2 = layers.MaxPool2D(2, strides=2)
-
-        # Flatten the data to a 1-D vector for the fully connected layer.
+        
         self.flatten = layers.Flatten()
 
-        self.fc1 = layers.Dense(1024, activation=tf.nn.relu)
+        self.fc1 = layers.Dense(512, activation=tf.nn.relu)
         
-        self.fc2 = layers.Dense(512, activation=tf.nn.relu)
+        self.fc2 = layers.Dense(100, activation=tf.nn.relu)
         # Apply Dropout (if is_training is False, dropout is not applied).
         # self.concat1 = layers.Concatenate()
 
