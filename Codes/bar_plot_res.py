0f0fa14b00d5cbeb89ee6eb37c0cae59adf81889
mxahan <smxahsan@gmail.com>
2021-04-08 11:52:00 -0400

    new

--- Codes/bar_plot_res.py
+++ Codes/bar_plot_res.py
@@ -377,3 +377,45 @@ plt.legend(loc='best', ncol=3, fontsize=30)
 # plt.savefig('bar_chart_sample3.eps', bbox_inches="tight", format = 'eps', dpi= 500)
 # 
 # plt.show()
+
+#%% Scatter plot 
+fig = plt.figure(figsize=(19.20,10.80))
+epoch =  [86, 86, 86]
+
+per_tr = [0.07, 0.08, 0.076]
+per_val = [0.12, .09, 0.11]
+
+mark_siz = 1300
+
+plt.scatter(epoch, per_tr, c = 'blue', marker = 'o', s = mark_siz, label = 'Personalized Train')
+plt.scatter(epoch, per_val, c = 'red', marker = 'o', s = mark_siz)
+
+
+plt.xticks(fontsize=35)
+plt.yticks(fontsize=35)
+
+
+epoch =  [16, 16, 16]
+tr_tr = [0.06, 0.065, 0.05]
+tr_val = [0.09, .091, 0.092]
+
+plt.scatter(epoch, tr_tr, c = 'blue', marker = '^', s = mark_siz, label = 'MTL')
+plt.scatter(epoch, tr_val, c = 'red', marker = '^', s = mark_siz)
+
+
+
+epoch =  [95, 95, 95]
+mtl_tr = [0.08, 0.085, 0.09]
+mtl_val = [0.1, .15, 0.12]
+
+plt.scatter(epoch, mtl_tr, c = 'blue',  marker = 'x', s = mark_siz, label = 'Tx Learning')
+plt.scatter(epoch, mtl_val, c = 'red',  marker = 'x', s = mark_siz)
+
+
+plt.xlabel('No of Epochs', fontweight="bold", fontsize=40)
+plt.ylabel('MSE', fontweight="bold", fontsize=40)
+plt.title('Validation vs Training MSE',fontsize=40, fontweight="bold")
+
+plt.legend(loc='best', ncol=1, fontsize=35)
+
+plt.savefig('tr_val_MSE.eps', bbox_inches="tight", format = 'eps', dpi= 500)
